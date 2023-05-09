425749716e6ae957aace894ff6ff9167f7976cbc
woori9 <wrh95222@gmail.com>
2023-05-09 23:00:22 +0900

    baekjoon 가장 긴 감소하는 부분 수열

---
+++ DP/가장 긴 감소하는 부분 수열/s1.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('input.txt', encoding='UTF8')
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1] * n
+
+for i in range(0, n):
+    for j in range(i + 1, n):
+        if arr[i] > arr[j]:
+            dp[j] = max(dp[j], dp[i] + 1)
+
+
+print(max(dp))
